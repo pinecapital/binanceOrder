f107fb991240e62574bce2448169e63d12f54c73
pinecapital <54320808+pinecapital@users.noreply.github.com>
2024-03-25 15:39:10 +0530

    addroute

--- test.py
+++ test.py
@@ -15,10 +15,36 @@ secret_key = os.getenv('SECRET_KEY')
 
 # Initialize Binance client for Spot trading
 client = Client(api_key, secret_key)
+app = Flask(__name__)
+ALLOWED_IPS = ['52.89.214.238', '34.212.75.30','54.218.53.128','52.32.178.7','127.0.0.1']
+@app.before_request
+def limit_remote_addr():
+    if request.remote_addr not in ALLOWED_IPS:
+        abort(403)  # Forbidden
 
-#get Account
-try:
-    accountinfo = client.get_account()
-    logging.info(f"getAccount: {accountinfo}")
-except BinanceAPIException as e:
-    logging.info(f"BinanceAPIException occurred: {str(e)}")
+@app.route('/balance', methods=['POST'])
+def balance():
+    data = request.get_json()
+    if not data:
+        logging.error("Missing data in request")
+        return jsonify({'error': 'Missing data'}), 400
+    # Log the entire incoming request data
+    logging.info(f"Incoming webhook data: {data}")
+
+    for order in data:
+        balance = order.get('balance')
+        if 'all' in balance:
+            #get Account
+            try:
+                accountinfo = client.get_account()
+                logging.info(f"getAccount: {accountinfo}")
+            except BinanceAPIException as e:
+                logging.info(f"BinanceAPIException occurred: {str(e)}")
+            except Exception as e:
+                # Catch any other exceptions that were not anticipated
+                logging.exception("Unexpected exception occurred")
+                return jsonify({'error': 'An unexpected error occurred'}), 500
+    return jsonify({'message': 'message processed'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=80)
